b2e63cc971b8f67458917c6005b49747ff7e6da8
sunhanbo <hanbo.sun@deephi.tech>
2018-12-27 16:02:06 +0800

    利用pytorch实现了一个网络的定义，同时规定了自己的网络框架的输入格式

---
+++ train_mnist.py
@@ -0,0 +1,45 @@
+#-*-coding:utf-8-*-
+import torch
+import torch.nn as nn
+import torchvision
+
+
+# 网络结构的定义，同时将网络结构写入文件中方便读取相关的参数
+# 参考了LeNet的设计，但是由于是在mnist数据集上进行测试，所以只有两层卷积+两层全连接
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 20, kernel_size = 5)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size = 2)
+        self.conv2 = nn.Conv2d(in_channels = 20, out_channels = 50, kernel_size = 5)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size = 2)
+        self.fc3 = nn.Linear(in_features = 4*4*50, out_features = 128)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(in_features = 128, out_features = 10)
+    def forward(x):
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = x.view(x.shape[0], -1)
+        x = self.relu3(self.fc3(x))
+        x = self.fc4(x)
+        return x
+net = LeNet()
+# 这部分将结果写到一个模型定义文件中去，目前准备设计的结构只支持单路径计算，
+# 对卷积不支持pad，不支持stride，非线性层只支持ReLU，Pooling不支持stride，要求长宽均能整除kernel_size
+# 只支持方形的卷积核和降采样核，Pooling支持MAX--0和AVE--1
+with open('layer_params.txt', 'w') as f:
+    # conv写入conv in_channels out_channels kernel_size 0/1(bias)
+    # relu写入relu
+    # pooling写入pooling kernel_size 0/1(max or ave)
+    # fc写入fc in_features out_features 0/1(bias)
+    f.write(f'conv {net.conv1.in_channels} {net.conv1.out_channels} {net.conv1.kernel_size[0]} {int(isinstance(net.conv1.bias, nn.Parameter))}\n')
+    f.write('relu\n')
+    f.write(f'pooling {net.pool1.kernel_size} 0\n')
+    f.write(f'conv {net.conv2.in_channels} {net.conv2.out_channels} {net.conv2.kernel_size[0]} {int(isinstance(net.conv2.bias, nn.Parameter))}\n')
+    f.write('relu\n')
+    f.write(f'pooling {net.pool2.kernel_size} 0\n')
+    f.write(f'fc {net.fc3.in_features} {net.fc3.out_features} {int(isinstance(net.fc3.bias, nn.Parameter))}\n')
+    f.write('relu\n')
+    f.write(f'fc {net.fc4.in_features} {net.fc4.out_features} {int(isinstance(net.fc4.bias, nn.Parameter))}\n')
